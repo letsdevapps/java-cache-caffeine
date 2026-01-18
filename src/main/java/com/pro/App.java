945b23fb2e70eb71c051378d0e0d8d800801971f
Default Name <letsdevapps@gmail.com>
2026-01-18 11:43:01 -0300

    Inclusao metricas stats

--- src/main/java/com/pro/App.java
+++ src/main/java/com/pro/App.java
@@ -1,19 +1,29 @@
 package com.pro;
 
+import com.github.benmanes.caffeine.cache.stats.CacheStats;
 import com.pro.cache.UserCache;
 import com.pro.repository.UserRepository;
 
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "----- Java Cache Caffeine | Main -----" );
-        
-        UserRepository repository = new UserRepository();
-        UserCache userCache = new UserCache(repository);
+	public static void main(String[] args) {
+		System.out.println("----- Java Cache Caffeine | Main -----");
 
-        System.out.println("Primeira chamada");
-        System.out.println(userCache.get(1).getNome());
+		UserRepository repository = new UserRepository();
+		UserCache userCache = new UserCache(repository);
 
-        System.out.println("Segunda chamada");
-        System.out.println(userCache.get(1).getNome());
-    }
+		System.out.println("Primeira chamada");
+		System.out.println(userCache.get(1).getNome());
+
+		System.out.println("Segunda chamada");
+		System.out.println(userCache.get(1).getNome());
+
+		System.out.println("----- Java Cache Caffeine | Metricas -----");
+
+		CacheStats stats = userCache.stats();
+
+		System.out.println("Hits: " + stats.hitCount());
+		System.out.println("Misses: " + stats.missCount());
+		System.out.println("Hit rate: " + stats.hitRate());
+		System.out.println("Load time (ms): " + stats.totalLoadTime() / 1_000_000);
+	}
 }
--- src/main/java/com/pro/cache/UserCache.java
+++ src/main/java/com/pro/cache/UserCache.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.github.benmanes.caffeine.cache.stats.CacheStats;
 import com.pro.model.User;
 import com.pro.repository.UserRepository;
 
@@ -13,12 +14,25 @@ public class UserCache {
 
 	public UserCache(UserRepository repository) {
 		this.cache = Caffeine.newBuilder()
-                .maximumSize(1_000)
-                .expireAfterWrite(5, TimeUnit.MINUTES)
-                .build(repository::buscarPorId);
+				.maximumSize(1_000)
+				.expireAfterWrite(5, TimeUnit.MINUTES)
+				.recordStats() // registrar metricas
+				.build(repository::buscarPorId);
 	}
 
 	public User get(Integer userId) {
 		return cache.get(userId);
+	}
+
+	public void invalidate(Integer userId) {
+		cache.invalidate(userId);
+	}
+
+	public void invalidateAll() {
+		cache.invalidateAll();
+	}
+
+	public CacheStats stats() {
+		return cache.stats();
 	}
 }
